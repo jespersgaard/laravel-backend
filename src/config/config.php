a3b822000c96fbaa8d0ed467bfb32bb90eebf3b7
JeroenG <jeroengjeroeng@gmail.com>
2014-03-07 22:03:15 +0100

    New file: config settings

---
+++ src/config/config.php
@@ -0,0 +1,21 @@
+<?php
+
+return array(
+
+	// Turn these on if you have those packages installed. The laravel-auth package (users) is included in this package.
+	'menuItems' => array(
+		'users' 	=> true,
+		'gallery' 	=> true,
+		'pages'		=> true,
+	),
+
+	// Here you can add your own menu items.
+	'customMenu' => array(
+		'Home' => array(
+			'route' => '/',
+			'text' 	=> 'Back to site',
+			'class' => 'item',
+		),
+	),
+
+);
